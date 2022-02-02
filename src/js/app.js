07c1c02312542a0bdd67d5b653a5abe67d870822
Shahriyor - Keypix <78787657+shahriyor-sharifjonov@users.noreply.github.com>
2022-02-02 15:17:07 +0500

    initial

--- src/js/app.js
+++ src/js/app.js
@@ -102,3 +102,92 @@ check.forEach(element => {
         next.classList.add('can'); 
     })
 });
+
+
+
+let activeQuiz2 = 1
+const quizTitle2 = document.querySelector('.quiz-2__title');
+const progress2 = document.querySelector('.quiz-2__progress-track');
+const prev2 = document.querySelector('.quiz-2__prev');
+const next2 = document.querySelector('.quiz-2__next');
+const item2 = document.querySelectorAll('.quiz-2__item');
+const check2 = document.querySelectorAll('.quiz-2__check');
+window.reloadQuiz2 = function(){
+    if(activeQuiz2 == 1){
+        quizTitle2.innerHTML = '1. Где планируете ремонт?'
+        prev2.classList.add('hidden');
+        progress2.style.width = '14.2857142857%';
+        item2.forEach(el => {
+            el.classList.remove('active');
+        });
+        document.querySelector('.quiz-2__1').classList.add('active');
+    }else if(activeQuiz2 == 2){
+        quizTitle2.innerHTML = 'Какой тип ремонта Вас интересует?'
+        prev2.classList.remove('hidden');
+        progress2.style.width = '28.5714285714%';
+        item2.forEach(el => {
+            el.classList.remove('active');
+        });
+        document.querySelector('.quiz-2__2').classList.add('active');
+    }else if(activeQuiz2 == 3){
+        quizTitle2.innerHTML = 'Какая площадь требуется к ремонту?'
+        prev2.classList.remove('hidden');
+        progress2.style.width = '42.8571428571%';
+        item2.forEach(el => {
+            el.classList.remove('active');
+        });
+        document.querySelector('.quiz-2__3').classList.add('active');
+    }else if(activeQuiz2 == 4){
+        quizTitle2.innerHTML = 'Есть ли у Вас дизайн проект?'
+        prev2.classList.remove('hidden');
+        progress2.style.width = '57.1428571428%';
+        item2.forEach(el => {
+            el.classList.remove('active');
+        });
+        document.querySelector('.quiz-2__4').classList.add('active');
+    }else if(activeQuiz2 == 5){
+        quizTitle2.innerHTML = 'Требуется ли Вам помощь с подбором материала?'
+        prev2.classList.remove('hidden');
+        progress2.style.width = '71.4285714285%';
+        item2.forEach(el => {
+            el.classList.remove('active');
+        });
+        document.querySelector('.quiz-2__5').classList.add('active');
+    }else if(activeQuiz2 == 6){
+        quizTitle2.innerHTML = 'Когда планируете начать ремонт?'
+        prev2.classList.remove('hidden');
+        progress2.style.width = '85.7142857142%';
+        item2.forEach(el => {
+            el.classList.remove('active');
+        });
+        document.querySelector('.quiz-2__6').classList.add('active');
+    }else if(activeQuiz2 == 7){
+        quizTitle2.innerHTML = 'Выберите Ваш подарок'
+        prev2.classList.remove('hidden');
+        progress2.style.width = '100%';
+        item2.forEach(el => {
+            el.classList.remove('active');
+        });
+        document.querySelector('.quiz-2__7').classList.add('active');
+        next2.type = "submit"
+    }
+}
+reloadQuiz2()
+next2.addEventListener('click', function(){
+    if(next2.classList.contains('can')){
+        activeQuiz2 ++;
+        reloadQuiz2()
+        next2.classList.remove('can');
+    }
+})
+prev2.addEventListener('click', function(){
+    activeQuiz2 --;
+    reloadQuiz2()
+    next2.classList.add('can');
+})
+
+check2.forEach(element => {
+    element.addEventListener('click', function(){
+        next2.classList.add('can'); 
+    })
+});
